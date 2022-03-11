21c035db8ca774dac4c1d04e9dd1bcc197447fad
MVAodhan <hamilton.aodhan@gmail.com>
2022-03-11 18:50:16 +1300

    progress I guess

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, WebhookClient } = require('discord.js');
 require('dotenv').config();
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
+const webhookClient = new WebhookClient(
+  {
+    id: process.env.WEBHOOK_ID,
+    token: process.env.WEBHOOK_TOKEN,
+  },
+  {
+    ws: 'ws://tau-usenameaodhan.up.railway.app/ws/twitch-events/',
+  }
+);
+
 client.on('ready', () => {
   console.log('Ready!');
 });
+
+webhookClient.send('hello from the webhook');
 
 client.on('messageCreate', (message) => {
   if (message.content === 'ping') {
