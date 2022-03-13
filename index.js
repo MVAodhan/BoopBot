911b8bf22bdb23c1d5d5c81ca1fb9b43cc851fe1
MVAodhan <hamilton.aodhan@gmail.com>
2022-03-13 22:11:17 +1300

    playing around with the recieved data

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ ws.on('open', () => {
   ws.send(JSON.stringify({ token: process.env.TAU_TOKEN }));
 });
 
-ws.on('message', function message(data) {
-  console.log('received: %s', data);
-});
-
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
@@ -27,6 +23,10 @@ client.on('ready', (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+ws.on('message', async (data) => {
+  const parsed = JSON.parse(data);
+  console.log(parsed.event_data.broadcaster_user_login, parsed.event_type);
+});
 // webhookClient.send('Webhook without client');
 
 client.on('messageCreate', (message) => {
